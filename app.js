019783cbd36ca283400bb9698f4a3abcdca79361
ruba-abu-yasein <abuyaseinruba@gmail.com>
2021-02-11 21:54:02 +0200

    problem3 solved

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ return [sum,'The sum of ' + a + ' and ' + b + ' is ' + sum + '.'];
 console.log(sum(4,7));
 
 // Here is the test for sum(); uncomment it to run it
-testSum(4, 7);
+// testSum(4, 7);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -37,7 +37,7 @@ function multiply(a, b) {
 console.log(multiply(5,9))
 
 // Here is the test for multiply(); uncomment it to run it
-testMultiply(5,9);
+// testMultiply(5,9);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -53,12 +53,19 @@ IMPORTANT DETAIL: You may not use the arithmetic operators + and * in this funct
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testSumAndMultiply() function and see if the test passes.*/
 
 // Write your code here
-function sumAndMultiply(a, b, c) {
+function sumAndMultiply(a, b, c) { 
   //eslint-disable-line
+  let sumReturn =  sum(a,b)[0];
+  sumReturn = sum(sumReturn,c)[0];
+  let multiplyReturn = multiply(a,b)[0];
+  multiplyReturn = multiply(multiplyReturn,c)[0];
+ return [sumReturn, multiplyReturn,  a + ' and ' + b + ' and '+ c + ' sum to ' + sumReturn + '.' , 'The product of ' + 4 + ' and ' + 7 + ' and ' + 5 + ' is '  + multiplyReturn + '.' ];
+
 }
+console.log(sumAndMultiply(4,7,5));
 
 // Here is the test for sumAndMultiply(); uncomment it to run it
-// testSumAndMultiply(4,7,5);
+//  testSumAndMultiply(4,7,5);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
